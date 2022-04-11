a0c86f8cc2f54342b0d6d436a81576aca1782951
Hassanaqve <hassanaqve@gmail.com>
2022-04-11 12:36:11 +0500

    front_end update

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,16 +1,43 @@
-const serverUrl = "https://s2zqdpynorlr.usemoralis.com:2053/server";
-const appId = "5ogtNDY2GiDXcN18ZkXJJHuParTFuUEMripUvqZz";
+
+//connecting to Moralis
+const serverUrl = "https://ayqxx7dwndh1.usemoralis.com:2053/server";
+const appId = "YF36G57FjBBdl0BD59EyBTLMS1lvNYIMLQq674sx";
 Moralis.start({ serverUrl, appId });
 
-let eId = document.getElementById('e-id')
-let eName = document.getElementById('e-name')
+//Fectching the data for new employee. 
+let eId = document.getElementById('e-id');
+let eName = document.getElementById('e-name');
+let eIdR = document.getElementById("e-idR");
+let eIdRM = document.getElementById("e-idRM");
+document.getElementById("addempform").addEventListener("submit", (e) => e.preventDefault());
+
+let eIdNew;
+let eNameNew;
+let eIdRNew;
+let eIdRMNew;
 
 function fetchInput() {
-    eId = eId.value;
-    eName = eName.value;
-    console.log(eId, eName)
+
+    eIdNew = eId.value;
+    eId.value = null;
+    eNameNew = eName.value;
+    eName.value = null;
+    console.log(eId, eName);
 }
 
+function fetchRetriveInput() {
+    eIdRNew = eIdR.value;
+    eIdR.value = null;
+    console.log(eIdRNew)
+}
+
+function fetchRemoveInput() {
+    eIdRMNew = eIdRM.value;
+    eIdRM.value = null;
+    console.log(eIdRMNew)
+}
+
+//Login button
 async function login() {
     let user = Moralis.User.current();
     if (!user) {
@@ -25,6 +52,7 @@ async function login() {
     }
 }
 
+//logout button
 async function logOut() {
     await Moralis.User.logOut();
     console.log("Logged Out")
@@ -36,16 +64,35 @@ document.getElementById("btn-logout").onclick = logOut;
 function goinTrough() {
     fetchInput();
     //addEmployee(eId, eName)
-    loadingdata(eId, eName);
+    loadingdata(eIdNew, eNameNew);
+    console.log(eIdNew, eNameNew)
+
+}
+function startingFetch() {
+    fetchRetriveInput();
+    readDataFromSc(eIdRNew);
+    console.log("in starting fetch")
 
 }
 
+function startingFetchRM() {
+    fetchRemoveInput();
+    deleteEmp(eIdRMNew);
+    console.log("in starting fetch delete")
+
+}
+
+
+
 const ABI = [{ "inputs": [{ "internalType": "uint8", "name": "_empId", "type": "uint8" }, { "internalType": "string", "name": "_name", "type": "string" }], "name": "addEmployee", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]
-const add = "0x337B7B6e72eDcD20AC601D005258F94d78e317f8"
+const add = "0x5fC75C7f7A4A5c74b8B5ee5b43233ca098b76843"
+const retriveABI = [{ "inputs": [{ "internalType": "uint8", "name": "_empId", "type": "uint8" }], "name": "retrieve", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }]
+const deleteABI = [{ "inputs": [{ "internalType": "uint8", "name": "_empId", "type": "uint8" }], "name": "removeEmployee", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]
 
 async function loadingdata(_eId, _eName) {
     const web3 = await Moralis.enableWeb3();
     try {
+        console.log("inside try block");
         const sendOptions = {
             contractAddress: add,
             functionName: "addEmployee",
@@ -58,18 +105,54 @@ async function loadingdata(_eId, _eName) {
 
         const transaction = await Moralis.executeFunction(sendOptions);
         console.log(transaction.hash);
-        // --> "0x39af55979f5b690fdce14eb23f91dfb0357cb1a27f387656e197636e597b5b7c"
-
-        // Wait until the transaction is confirmed
         await transaction.wait();
-
-        // Read new value
-        const message = await Moralis.executeFunction(readOptions);
-        console.log(message);
-        // --> "Hello Moralis"
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-    console.log("No error")
+    console.log("No error");
+}
+document.querySelector('#fetchForm').addEventListener("submit", (e) => e.preventDefault())
+
+async function readDataFromSc(_eIdR) {
+    const web3 = await Moralis.enableWeb3();
+    console.log("inside fetch function");
+
+    const readOptions = {
+        contractAddress: add,
+        functionName: "retrieve",
+        abi: retriveABI,
+        params: {
+            _empId: _eIdR,
+        }
+    }
+    console.log("before fatching")
+    const message = await Moralis.executeFunction(readOptions);
+    let ans = document.getElementById("output");
+    ans.innerHTML = "Employee name is " + message;
+    console.log(message);
+    console.log("at the end of the function");
+}
+
+
+
+//deleting the employe record
+document.querySelector('#DeleteEmp').addEventListener("submit", (e) => e.preventDefault())
+
+async function deleteEmp(_eIdR) {
+    const web3 = await Moralis.enableWeb3();
+    console.log("inside fetch function");
+
+    const readOptions = {
+        contractAddress: add,
+        functionName: "removeEmployee",
+        abi: deleteABI,
+        params: {
+            _empId: _eIdR,
+        }
+    }
+    console.log("before fatching")
+    const message = await Moralis.executeFunction(readOptions);
+    console.log(message);
+    console.log("at the end of the function");
 }
 
